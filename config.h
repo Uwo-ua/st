4861a0477fe9a65837d27e9aa4972d1142e23310
Uwo-ua <tovstolaki62@gmail.com>
2022-10-11 21:00:43 +0300

    Setup

--- config.h
+++ config.h
@@ -54,7 +54,7 @@ unsigned int tabspaces = 4;
 /* bg opacity */
 float alpha = 0.9;
 float alphaOffset = 1; 
-float alphaUnfocus = 1.0;
+float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
